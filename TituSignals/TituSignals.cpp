89b1342d5263f32a33958bdec50ecdc117121d8e
BlayXavi <blay.xavi@gmail.com>
2022-04-21 16:51:44 +0200

    Added Context functionality && Code Maintenance

--- TituSignals/TituSignals.cpp
+++ TituSignals/TituSignals.cpp
@@ -26,6 +26,12 @@ public:
 		std::cout << "PrintParams: " << i << " " << j << "  " << k << " ---- Instance Values: " << m_Int << " " << m_Float << std::endl;
 	}
 
+	void PrintWithContext(int i, int j, float k, void* Context)
+	{
+		float* reinterpretedContext = (float*)(Context); //retreive context
+		std::cout << "PrintParams: " << i << " " << j << "  " << k << " ---- Instance Values: " << m_Int << " " << m_Float << "------ Context: " << *reinterpretedContext << std::endl;
+	}
+
 	static void PrintParamsStatic()
 	{
 		std::cout << "Static Call" << std::endl;
@@ -43,20 +49,24 @@ int main()
 {
 	//Empty Signals
 	ClassA classA;
-	int lambdaHandler = classA.m_OnValueChanged.AddListener(([&](int a, float b) -> void {std::cout << "Lambda Example " << a << "  " << b << std::endl; }));
+	uint32_t lambdaHandler = classA.m_OnValueChanged.AddListener(([&](int a, float b) -> void {std::cout << "Lambda Example " << a << "  " << b << std::endl; }));
 
 	Signal<> jSignal;
-	int emptySignalHandler = jSignal.AddListener(&classA, &ClassA::Print); //no method parameters example
-	int staticHandler = jSignal.AddListener(&ClassA::PrintParamsStatic); //static method example
+	uint32_t emptySignalHandler = jSignal.AddListener(&classA, &ClassA::Print); //no method parameters example
+	uint32_t staticHandler = jSignal.AddListener(&ClassA::PrintParamsStatic); //static method example
 	jSignal.AddListener(HelloWorld); //global function example
 	jSignal.Dispatch(); //dispatch with no parameters
 
 	//Signals with parameters
 	Signal<int, int, float> jSignal2;
-	int parameterSignalHandler = jSignal2.AddListener(&classA, &ClassA::Print); //method parameters example
+	uint32_t parameterSignalHandler = jSignal2.AddListener(&classA, &ClassA::Print); //method parameters example
 	jSignal2.Dispatch(1, 2, 3.4f); //parameters dispacth
 	classA.SetInt(12345);//modify Instance
-	jSignal2.Dispatch(1, 2, 3.4f); //se changes on instance
+	jSignal2.Dispatch(1, 2, 3.4f); //see changes on instance
+
+	float Context = 4.5f;
+	uint32_t parameterSignalHandlerContext = jSignal2.AddListener(&classA, &ClassA::PrintWithContext, (void*)(&Context)); //Stored context to retreive it later regardless of what Signal is Dispatching 
+	jSignal2.Dispatch(1, 2, 3.0f); //Example how Context will be reitreived independently of Signal parameters
 
 	auto handler = jSignal2.GetListener(parameterSignalHandler); //how to retrieve function pointer whit handler
 
@@ -65,6 +75,7 @@ int main()
 	jSignal.RemoveListener(staticHandler);
 	classA.m_OnValueChanged.RemoveListener(lambdaHandler);
 	jSignal2.RemoveListener(parameterSignalHandler);
+	jSignal2.RemoveListener(parameterSignalHandlerContext);
 
 	return 0;
 }
--- include/Signals.h
+++ include/Signals.h
@@ -14,11 +14,12 @@ class Signal
 public:
 
 	typedef std::function<void(Args...)> Listener;
+	typedef std::function<void(Args..., void*)> ListenerContext;
 
 	virtual ~Signal() { m_Listeners.clear(); }
 
 	//add member method as listener. Store returned integer (handler) to remove the same listener
-	int AddListener(const Listener& func)
+	uint32_t AddListener(const Listener& func)
 	{
 		m_Listeners.insert(std::make_pair(++m_HandlerCounter, func));
 		return m_HandlerCounter;
@@ -26,7 +27,7 @@ public:
 
 	//add member method as listener. Store returned integer (handler) to remove the same listener
 	template<typename T>
-	int AddListener(T* inst, void (T::* func)(Args...))
+	uint32_t AddListener(T* inst, void (T::* func)(Args...))
 	{
 		return AddListener([=](Args...args) -> void
 			{
@@ -36,7 +37,7 @@ public:
 
 	//add member method as listener. Store returned integer (handler) to remove the same listener
 	template<typename T>
-	int AddListener(T* inst, void (T::* func)(Args...) const)
+	uint32_t AddListener(T* inst, void (T::* func)(Args...) const)
 	{
 		return AddListener([=](Args...args) -> void
 			{
@@ -44,15 +45,52 @@ public:
 			});
 	}
 
+	uint32_t AddListener(const ListenerContext& func, void* Context)
+	{
+		m_ListenersContext.insert(std::make_pair(++m_HandlerCounter, std::make_pair(func, Context)));
+		return m_HandlerCounter;
+	}
+
+	//add member method as listener. Store returned integer (handler) to remove the same listener
+	template<typename T>
+	uint32_t AddListener(T* inst, void (T::* func)(Args..., void*), void* Context)
+	{
+		return AddListener([=](Args...args, void* C) -> void
+			{
+				(inst->*func)(args..., C);
+			}, Context);
+	}
+
+	//add member method as listener. Store returned integer (handler) to remove the same listener
+	template<typename T>
+	uint32_t AddListener(T* inst, void (T::* func)(Args..., void*) const, void* Context)
+	{
+		return AddListener([=](Args...args, void* C) -> void
+			{
+				(inst->*func)(args..., C);
+			}, Context);
+	}
+
 	void RemoveListener(const int& i)
 	{
 		m_Listeners.erase(i);
+		m_ListenersContext.erase(i);
+	}
+
+	void RemoveAllListeners()
+	{
+		m_Listeners.clear();
+		m_ListenersContext.clear();
 	}
 
 	void Dispatch(const Args& ... args)
 	{
 		for (auto const& it : m_Listeners) {
 			it.second(std::forward<decltype(args)>(args)...);
+		}
+
+		for (auto const& it : m_ListenersContext) {
+			it.second.first(std::forward<decltype(args)>(args)..., it.second.second);
 		}
 	}
 
@@ -61,7 +99,14 @@ public:
 		return m_Listeners[i];
 	}
 
+	ListenerContext GetListenerContext(const int& i)
+	{
+		return m_ListenersContext[i];
+	}
+
 private:
 	std::map<int, Listener> m_Listeners;
+	std::map<uint32_t, std::pair<ListenerContext, void*>> m_ListenersContext;
+
 	uint32_t m_HandlerCounter = 0;
 };
