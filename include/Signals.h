8b94c70465bc849d8efc894eb2a2e76a0d0452b0
BlayXavi <blay.xavi@gmail.com>
2023-03-09 16:52:26 +0100

    Added OnceListener funcionalities && Fixed compilation issues when Listerner is subscribed to a Signal from a const function

--- include/Signals.h
+++ include/Signals.h
@@ -1,12 +1,10 @@
-//Code made by Xavier Blay. https://github.com/BlayXavi
-
-#pragma once
+﻿#pragma once
 #include <map>
 #include <functional>
 
 //USAGE:
 //Signal signal;
-//signal.AddListener(HelloWorld); //To Add Global/Static Function
+//signal.AddListener(&Class::DoSomething); //To Add Global/Static Function
 //int i = signal.AddListener(&classAInstance, &ClassA::DoSomething); // To add member instance method
 template<class... Args>
 class Signal
@@ -14,7 +12,7 @@ class Signal
 public:
 
 	typedef std::function<void(Args...)> Listener;
-	typedef std::function<void(Args..., void*)> ListenerContext;
+	typedef std::function<void(Args..., uint8*)> ListenerContext;
 
 	virtual ~Signal() { m_Listeners.clear(); }
 
@@ -25,62 +23,122 @@ public:
 		return m_HandlerCounter;
 	}
 
-	//add member method as listener. Store returned integer (handler) to remove the same listener
-	template<typename T>
-	uint32_t AddListener(T* inst, void (T::* func)(Args...))
+	uint32_t AddListenerOnce(const Listener& func)
 	{
-		return AddListener([=](Args...args) -> void
-			{
-				(inst->*func)(args...);
-			});
+		m_ListenersOnce.insert(std::make_pair(++m_HandlerCounter, func));
+		return m_HandlerCounter;
 	}
 
-	//add member method as listener. Store returned integer (handler) to remove the same listener
-	template<typename T>
-	uint32_t AddListener(T* inst, void (T::* func)(Args...) const)
-	{
-		return AddListener([=](Args...args) -> void
-			{
-				(inst->*func)(args...);
-			});
-	}
-
-	uint32_t AddListener(const ListenerContext& func, void* Context)
+	uint32_t AddListener(const ListenerContext& func, uint8* Context)
 	{
 		m_ListenersContext.insert(std::make_pair(++m_HandlerCounter, std::make_pair(func, Context)));
 		return m_HandlerCounter;
 	}
 
-	//add member method as listener. Store returned integer (handler) to remove the same listener
-	template<typename T>
-	uint32_t AddListener(T* inst, void (T::* func)(Args..., void*), void* Context)
+	uint32_t AddListenerOnce(const ListenerContext& func, uint8* Context)
 	{
-		return AddListener([=](Args...args, void* C) -> void
-			{
-				(inst->*func)(args..., C);
-			}, Context);
+		m_ListenersContextOnce.insert(std::make_pair(++m_HandlerCounter, std::make_pair(func, Context)));
+		return m_HandlerCounter;
 	}
 
 	//add member method as listener. Store returned integer (handler) to remove the same listener
 	template<typename T>
-	uint32_t AddListener(T* inst, void (T::* func)(Args..., void*) const, void* Context)
+	uint32_t AddListener(const T* inst, void (T::* func)(Args...))
 	{
-		return AddListener([=](Args...args, void* C) -> void
+		return AddListener([=](Args...args) -> void
 			{
-				(inst->*func)(args..., C);
+				(const_cast<T*>(inst)->*func)(args...);
+			});
+	}
+
+	template<typename T>
+	uint32_t AddListenerOnce(const T* inst, void (T::* func)(Args...))
+	{
+		return AddListenerOnce([=](Args...args) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args...);
+			});
+	}
+
+	//add member method as listener. Store returned integer (handler) to remove the same listener
+	template<typename T>
+	uint32_t AddListener(const T* inst, void (T::* func)(Args...) const)
+	{
+		return AddListener([=](Args...args) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args...);
+			});
+	}
+
+	template<typename T>
+	uint32_t AddListenerOnce(const T* inst, void (T::* func)(Args...) const)
+	{
+		return AddListenerOnce([=](Args...args) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args...);
+			});
+	}
+
+	//add member method as listener. Store returned integer (handler) to remove the same listener
+	template<typename T>
+	uint32_t AddListener(const T* inst, void (T::* func)(Args..., uint8*), uint8* Context)
+	{
+		return AddListener([=](Args...args, uint8* C) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args..., C);
 			}, Context);
 	}
 
+	template<typename T>
+	uint32_t AddListenerOnce(const T* inst, void (T::* func)(Args..., uint8*), uint8* Context)
+	{
+		return AddListenerOnce([=](Args...args, uint8* C) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args..., C);
+			}, Context);
+	}
+
+	
+	//add member method as listener. Store returned integer (handler) to remove the same listener
+	template<typename T>
+	uint32_t AddListener(const T* inst, void (T::* func)(Args..., uint8*) const, uint8* Context)
+	{
+		return AddListener([=](Args...args, uint8* C) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args..., C);
+			}, Context);
+	}
+
+	template<typename T>
+	uint32_t AddListenerOnce(const T* inst, void (T::* func)(Args..., uint8*) const, uint8* Context)
+	{
+		return AddListenerOnce([=](Args...args, uint8* C) -> void
+			{
+				(const_cast<T*>(inst)->*func)(args..., C);
+			}, Context);
+	}
+	
 	void RemoveListener(const int& i)
 	{
 		m_Listeners.erase(i);
 		m_ListenersContext.erase(i);
+
+		m_ListenersOnce.erase(i);
+		m_ListenersContextOnce.erase(i);
 	}
 
 	void RemoveAllListeners()
 	{
 		m_Listeners.clear();
 		m_ListenersContext.clear();
+
+		m_ListenersOnce.clear();
+		m_ListenersContextOnce.clear();
+	}
+
+	void RemoveListeners()
+	{
+		RemoveAllListeners();
 	}
 
 	void Dispatch(const Args& ... args)
@@ -89,9 +147,19 @@ public:
 			it.second(std::forward<decltype(args)>(args)...);
 		}
 
-		for (auto const& it : m_ListenersContext) {
-			it.second.first(std::forward<decltype(args)>(args)..., it.second.second);
+		for (auto const& it : m_ListenersContext){
+			it.second.first(std::forward<decltype(args)>(args)..., it.second.second);	
 		}
+
+		for (auto const& it : m_ListenersOnce) {
+			it.second(std::forward<decltype(args)>(args)...);
+		}
+		m_ListenersOnce.clear();
+
+		for (auto const& it : m_ListenersContextOnce){
+			it.second.first(std::forward<decltype(args)>(args)..., it.second.second);	
+		}
+		m_ListenersContextOnce.clear();
 	}
 
 	Listener GetListener(const int& i)
@@ -99,14 +167,11 @@ public:
 		return m_Listeners[i];
 	}
 
-	ListenerContext GetListenerContext(const int& i)
-	{
-		return m_ListenersContext[i];
-	}
-
 private:
-	std::map<int, Listener> m_Listeners;
-	std::map<uint32_t, std::pair<ListenerContext, void*>> m_ListenersContext;
-
+	std::map<uint32_t, Listener> m_Listeners;
+	std::map<uint32_t, Listener> m_ListenersOnce;
+	std::map<uint32_t, std::pair<ListenerContext, uint8_t*>> m_ListenersContext;
+	std::map<uint32_t, std::pair<ListenerContext, uint8_t*>> m_ListenersContextOnce;
+	
 	uint32_t m_HandlerCounter = 0;
 };
